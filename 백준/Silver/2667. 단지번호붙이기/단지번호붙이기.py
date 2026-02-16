76324f2326a7e2e44fdf79edaf5a71d66841845d
eyeol <42065198+eyeol@users.noreply.github.com>
2026-02-16 18:25:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -16,13 +16,10 @@ def solution():
     # 단지수 ; bfs tree 개수
     # 각 단지에 속하는 집의 수; visited가 갱신될 때마다 1씩 더하면 됨
 
-    def bfs(x, y, tree_num):
+    def bfs(x, y):
         q = deque([(x, y)])
         visited[x][y] = 1
         house_num = 1
-
-        board[x][y] = tree_num
-
 
         while q:
             cx, cy = q.popleft()
@@ -37,8 +34,6 @@ def solution():
                         q.append((nx, ny))
                         visited[nx][ny] = 1
                         house_num += 1
-                        # 이 부분 없어도 될듯
-                        board[nx][ny] = tree_num
 
         return house_num
 
@@ -51,7 +46,7 @@ def solution():
             # 집이 있고 / 방문한 적 없으면 거기서 bfs 시작
             if board[x][y] != 0 and not visited[x][y]:
                 tree_num += 1
-                result = bfs(x, y, tree_num)
+                result = bfs(x, y)
                 house_num.append(result)
 
     print(tree_num)
