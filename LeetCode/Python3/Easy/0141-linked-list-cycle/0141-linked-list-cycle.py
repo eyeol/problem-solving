a8650e8d4257748b4a5930a2cc165c9e9b6e3745
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-13 16:16:54 +0900

    Time: 64 ms (6.66%), Space: 23.1 MB (8.27%) - LeetHub

---
+++ LeetCode/Python3/Easy/0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        visited = set()
+        curr = head
+
+        while curr:
+            if curr in visited:
+                return True
+            # 방문한적 없으면
+            visited.add(curr)
+            curr = curr.next
+
+        return False
