1186af6158f0c1c771f008557fceb7b49dfaf13d
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-17 04:54:41 +0900

    [Gold V] Title: 토마토, Time: 668 ms, Memory: 218720 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,66 +3,51 @@ from collections import deque
 
 input = sys.stdin.readline
 
-DX = [1, 0, 0, -1, 0, 0]
-DY = [0, 1, 0, 0, -1, 0]
-DZ = [0, 0, 1, 0, 0, -1]
+DX = [1, -1, 0, 0, 0, 0]
+DY = [0, 0, 1, -1, 0, 0]
+DZ = [0, 0, 0, 0, 1, -1]
 
 def solution():
-
     M, N, H = map(int, input().split())
+    boxes = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
-    boxes = []
+    q = deque()
+    zero_cnt = 0
 
-    # box 개수는 H만큼
-    for _ in range(H):
-        # M x N 크기 박스
-        box = [list(map(int, input().split())) for _ in range(N)]
-        boxes.append(box)
-
-    # bfs 출발에 쓰일 토마토들
-    sources = []
-    all_riped = 1
     for z in range(H):
         for y in range(N):
             for x in range(M):
-                all_riped *= boxes[z][y][x]
-                if boxes[z][y][x] == 1:
-                    sources.append((x, y, z))
-    
-    # 모든 토마토가 있었다면
-    if all_riped != 0:
-        print("0")
-        return # early return
+                v = boxes[z][y][x]
+                if v == 1:
+                    q.append((x, y, z))
+                elif v == 0:
+                    zero_cnt += 1
 
+    if zero_cnt == 0:
+        print(0)
+        return
 
-    q = deque(sources)
-    # bfs
+    max_day = 1
+
     while q:
         cx, cy, cz = q.popleft()
         cur = boxes[cz][cy][cx]
+
         for k in range(6):
             nx = cx + DX[k]
             ny = cy + DY[k]
             nz = cz + DZ[k]
-            # 범위 체크
-            if 0<=nx<M and 0<=ny<N and 0 <=nz<H:
-                nxt = boxes[nz][ny][nx]
-                # nxt tomato가 0이면
-                if nxt == 0:
-                    boxes[nz][ny][nx] = cur + 1
-                    q.append((nx, ny, nz))
-    
-    max_val = 0
-    for z in range(H):
-        for y in range(N):
-            for x in range(M):
-                if boxes[z][y][x] == 0:
-                    print(-1)
-                    return
-                if boxes[z][y][x] > max_val:
-                    max_val = boxes[z][y][x]
 
-    print(max_val - 1)
+            if 0 <= nx < M and 0 <= ny < N and 0 <= nz < H and boxes[nz][ny][nx] == 0:
+                boxes[nz][ny][nx] = cur + 1
+                max_day = cur + 1
+                zero_cnt -= 1
+                q.append((nx, ny, nz))
+
+    if zero_cnt > 0:
+        print(-1)
+    else:
+        print(max_day - 1)
 
 if __name__ == "__main__":
     solution()
