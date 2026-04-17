3a661cf2f33440e8ca6636a428c1c772bc5d1ce0
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-17 18:56:48 +0900

    [Silver I] Title: IOIOI, Time: 120 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -7,49 +7,23 @@ def solution():
     M = int(input())
     S = input().strip()
 
-    # N의 범위 때문에 완전 탐색 불가능
-    # IO가 반복되면서 O가 N보다 길면 추적 시작
-
-    longer = []
-
-    tracking = False
-    forward = None
-    i_cnt = 0
-
-    for i in range(M):
-        if tracking:
-            # 다르면 트래킹 계속
-            if forward != S[i]:
-                forward = S[i]
-                # 트래킹 중 I 나오면 카운트 추가
-                if S[i] == "I":
-                    i_cnt += 1
-            # 같으면 트래킹 종료
-            else:
-                tracking = False
-                # 트래킹 종료 후 P_N 이상이면 기록
-                if i_cnt >= N+1:
-                    longer.append(i_cnt)
-
-        # 트래킹 시작할지 안할지 여부
-        if not tracking:
-            if S[i] == "I":
-                # 새로운 트래킹 시작
-                tracking = True
-                forward = "I"
-                # 카운트 초기화
-                i_cnt = 1
-
-    # 트래킹하다가 끝나면 마지막 기록 추가
-    if tracking and i_cnt >= N+1:
-        longer.append(i_cnt)
-    
     ans = 0
-    for length in longer:
-        ans += (length - N)
+    i = 0
+
+    while i <= M - 3:
+        if S[i] == "I" and S[i+1] == 'O' and S[i+2] == "I":
+            # 첫번째 i만 카운트
+            cnt = 1
+            while i+2 < M and S[i+1] == "O" and S[i+2] == "I":
+                cnt += 1
+                i += 2 # 2칸씩 전진하면서 탐색
+            if cnt > N:
+                ans += cnt - N
+            i += 1
+        else: 
+            i += 1
 
     print(ans)
-
 
 if __name__ == "__main__":
     solution()
