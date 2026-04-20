a8bede95225bd6705ef9741f49f0775b3dcfffe4
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-20 21:25:45 +0900

    [Gold III] Title: 파티, Time: 148 ms, Memory: 112160 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -16,10 +16,8 @@ def solution():
 
     # X에서 출발해서 노드별 도달하는 최단거리를 구해야 함 = BFS
     # 그런데 간선에 길이 정보가 있음 = 우선순위 큐로 다익스트라
-
     
-    
-    def dijkstra(u):
+    def dijkstra(u, adj):
         min_dist = [float("INF")] * (N+1)
         h = [(0, u)]
         min_dist[u] = 0
@@ -36,27 +34,10 @@ def solution():
 
         return min_dist
     
-    def r_dijkstra(u):
-        min_dist = [float("INF")] * (N+1)
-        h = [(0, u)]
-        min_dist[u] = 0
-
-        while h:
-            d, u = heapq.heappop(h)
-            if d > min_dist[u]:
-                continue
-            for v, w in radj[u]:
-                nd = d + w
-                if nd < min_dist[v]:
-                    min_dist[v] = nd
-                    heapq.heappush(h, (nd, v))
-
-        return min_dist
-    
     max_val = 0
 
-    dist_go = dijkstra(X)
-    dist_come = r_dijkstra(X)
+    dist_go = dijkstra(X, adj)
+    dist_come = dijkstra(X, radj)
 
     for i in range(1, N+1):
         if dist_go[i] != float("INF") and dist_come[i] != float("INF"):
