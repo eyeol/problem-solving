cac1ad280026ac039d1474379dd60166ca005015
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-21 03:58:52 +0900

    [Silver III] Title: N과 M (6), Time: 88 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,40 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    N, M = map(int, input().split())
+    nums = list(map(int, input().split()))
+
+    nums.sort()
+
+    result = []
+
+    #visited = [0] * N
+
+    def backtrack():
+        if len(result) == M:
+            print(*result)
+            return
+        
+        for i, num in enumerate(nums):
+            if result and num <= result[-1]:
+                continue
+
+            # 방문 처리
+            #visited[i] = 1
+            result.append(num)
+            
+            # 재귀
+            backtrack()
+
+            # 재귀 후 상태 복구
+            #visited[i] = 0
+            result.pop()
+    
+    backtrack()
+
+
+if __name__ == "__main__":
+    solution()
