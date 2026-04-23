a7ba84048581521f84fd6822300febbbe9665731
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-23 23:14:09 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 111744 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,12 +1,27 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+
+    triangle = [list(map(int, input().split())) for _ in range(N)]
+    dp = [[0]*N for _ in range(N)]
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, N):
+        for j in range(i+1):
+            # 맨 왼쪽
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            # 맨 오른쪽
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+    
+    print(max(dp[N-1]))
+
+
+if __name__ == "__main__":
+    solution()
