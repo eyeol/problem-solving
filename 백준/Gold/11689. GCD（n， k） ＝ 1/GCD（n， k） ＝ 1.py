853930fdef6574b552e62094b377a91d98bffa35
eyeol <42065198+eyeol@users.noreply.github.com>
2026-04-26 06:44:19 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 104 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    n = int(input())
+    result = n
+    temp = n
+    p = 2
+
+    while p * p <= temp:
+        if temp % p == 0:
+            while temp % p == 0:
+                temp //= p
+            result -= result // p
+        p += 1
+
+    if temp > 1:
+        result -= result // temp
+
+    print(result)
+
+if __name__ == "__main__":
+    solution()
